9f3b3bf3cc6f9124cfabffa86ca08d6f101f492e
Muhammad Khasanboev <m.khasanboev@tadi.uz>
2026-02-01 20:33:59 +0500

    MainActivity: unused imports deleted

--- app/src/main/java/com/example/quizify_v2/MainActivity.kt
+++ app/src/main/java/com/example/quizify_v2/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.quizify_v2.ui.theme.Quizifyv2Theme
 
 class MainActivity : ComponentActivity() {
